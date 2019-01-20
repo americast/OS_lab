34645d74596765dc7e5d488689c3783a2e0c6aa5
americast <sayan.sinha@iitkgp.ac.in>
2019-01-20 19:02:28 +0530

    Break into function

--- Assgn2/Assgn2_42.c
+++ Assgn2/Assgn2_42.c
@@ -6,6 +6,94 @@
 #include<sys/wait.h> 
 #include<fcntl.h>
 
+int execute(char *args[100])
+{
+
+	char in_file[100], out_file[100];
+	int i, flag_in = 0, flag_out =0, flag = 1, wait_flag = 0, p_start = 0, p_end =0, p = 0;
+	for (i = 0; i < 100 && args[i]; i++)
+	{
+		// printf("i is: %d\n", i);
+		if (strcmp(args[i], "<") == 0)
+		{
+			if(flag_out==1)
+			{	fprintf(stderr, "Wrong command %s\n" );
+				return(1);
+			}
+			flag_in = 1;
+			flag = 0;
+		}	
+		else if(strcmp(args[i], ">") == 0)
+		{
+			if(flag_in==1)
+			{	fprintf(stderr, "Wrong command %s\n" );
+				return(1);
+			}
+			flag_out = 1;
+			flag = 0;
+		}
+		else if(flag_in==1)
+		{
+			strcpy(in_file, args[i]);
+			flag_in = 2;
+		}
+		else if(flag_out==1)
+		{
+			strcpy(out_file, args[i]);
+			flag_out = 2;
+		}
+		else
+		{
+			if(strcmp(args[i],"&")==0)
+			{
+				wait_flag = 1;
+			}
+			if(!flag)
+			{
+				args[i] = NULL;
+			}
+		}
+		// printf("Command %d: %s\n", i, args[i]);
+	}
+
+	p = fork(); 	// spawning
+	
+	if (p < 0)
+	{ 
+	    fprintf(stderr, "Fork Failed" ); 
+	    // return 1; 
+	} 
+	else if (p == 0)
+	{
+		if(flag_in)
+		{
+			int f_in = open(in_file, O_RDONLY);
+			if(f_in<0)
+			{
+				perror("Input file error ");
+				exit(EXIT_FAILURE);
+			}
+			dup2(f_in,0);
+		}
+		if(flag_out)
+		{
+			int f_out = open(out_file, O_WRONLY| O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+			if(f_out<0)
+			{
+				perror("Output file error ");
+				exit(EXIT_FAILURE);
+			}
+			dup2(f_out,1);
+		}
+		execvp(args[0],args); 		// replace the child process with an example process
+		perror("There was an error");
+		exit(EXIT_FAILURE);
+	}
+	
+	return wait_flag;
+
+}
+
 int main()
 {
 	int i;
@@ -46,75 +134,8 @@ int main()
 		for (; i < 100; i++)
 			args[i] = NULL;
 
-		char in_file[100], out_file[100];
-		int flag_in = 0, flag_out =0, flag = 1;
-		for (i = 0; i < 100 && args[i]; i++)
-		{
-			// printf("i is: %d\n", i);
-			if (strcmp(args[i], "<") == 0)
-			{
-				if(flag_out==1)
-				{	fprintf(stderr, "Wrong command %s\n" );
-					return(1);
-				}
-				flag_in = 1;
-				flag = 0;
-			}	
-			else if(strcmp(args[i], ">") == 0)
-			{
-				if(flag_in==1)
-				{	fprintf(stderr, "Wrong command %s\n" );
-					return(1);
-				}
-				flag_out = 1;
-				flag = 0;
-			}
-			else if(flag_in)
-			{
-				strcpy(in_file, args[i]);
-				flag_in = 0;
-			}
-			else if(flag_out)
-			{
-				strcpy(out_file, args[i]);
-				flag_out = 0;
-			}
-			else
-			{
-				if(!flag)
-				{
-					args[i] = NULL;
-				}
-			}
-			// printf("Command %d: %s\n", i, args[i]);
-		}
-
-
-
-		p = fork(); 	// spawning
-		
-		if (p < 0)
-		{ 
-		    fprintf(stderr, "Fork Failed" ); 
-		    // return 1; 
-		} 
-		else if (p == 0)
-		{
-			int f_in = open(in_file, O_RDONLY);
-			if(f_in<0)
-			{
-				perror("Input file error ");
-				exit(EXIT_FAILURE);
-			}
-			dup2(f_in,0);
-			int f_out = open(out_file, O_WRONLY| O_CREAT);
-			dup2(f_out,1);
-			execvp(args[0],args); 		// replace the child process with an example process
-			perror("There was an error");
-			return 0;
-		}
-			// printf("PID: %d\n", p);
-		wait(NULL);
+		int wait_flag = execute(args);
+		if(!wait_flag) wait(NULL);
 		for (i = 0; i < 100; i++)
 			free(args[i]);
 	}
