a5c9289f7a114ac743061ff72641e823dd309f22
kodercat <swastika.d.108@gmail.com>
2019-02-10 11:34:52 +0530

    Update

--- Assgn4/Ass4_42.cpp
+++ Assgn4/Ass4_42.cpp
@@ -9,12 +9,23 @@ using namespace std;
 #define QUANTUM 1
 #define N 10
 
+struct threads
+{
+	pthread_t tid;
+	int id;
+	// int status;
+	char worker;
+};
+
 int BUFFER[M];
 int pos = 0;
 pthread_t P_threads[N];
 pthread_t C_threads[N];
-vector<pthread_t> ready_queue;
+pthread_t scheduler;
+pthread_t reporter;
+vector<threads> ready_queue;
 int num_p, num_c;
+vector<int> status(N);
 
 void catcher(int signum)
 {
@@ -22,15 +33,20 @@ void catcher(int signum)
     {
         sigset_t myset;
         sigemptyset(&myset);
+
         printf("Suspending thread\n");
+
         sigsuspend(&myset);
     }
     else
     {
         printf("Waking thread\n");
+
         return;
     }
 }
+
+
 
 void* producer(void *param)
 {
@@ -39,22 +55,25 @@ void* producer(void *param)
     
     int i = 0;
     while(i < 1000)
+    {
         if (pos < M-1)
         {
             pos++;
             BUFFER[pos] = rand() % 1000;
             i+=1;
         }
-
-    vector<pthread_t>::iterator it;
-    for (it = ready_queue.begin(); it != ready_queue.end(); it++, i++)
-    {
-        if (ready_queue[i] == pthread_self())
-        {
-            ready_queue.erase(ready_queue.begin() + i);
-            break;
-        }
     }
+
+    for(int i = 0; i<ready_queue.size();i++)
+    {
+    	if (ready_queue[i].tid == pthread_self())
+    	{
+    		printf("Removing %c%d\n", ready_queue[i].worker, ready_queue[i].id);
+    	    ready_queue.erase(ready_queue.begin() + i);
+    	    break;
+    	}
+    }
+    pthread_exit(0);
 }
 
 void* consumer(void *param)
@@ -68,50 +87,142 @@ void* consumer(void *param)
             pos--;
     }
 
-    vector<pthread_t>::iterator it;
-    int i = 0;
-    for (it = ready_queue.begin(); it != ready_queue.end(); it++, i++)
+    // vector<pthread_t>::iterator it;
+    // int i = 0;
+    // for (it = ready_queue.begin(); it != ready_queue.end(); it++, i++)
+    // {
+    //     if (ready_queue[i] == pthread_self())
+    //     {
+    //         ready_queue.erase(ready_queue.begin() + i);
+    //         break;
+    //     }
+    // }
+
+    for(int i = 0; i< ready_queue.size();i++)
     {
-        if (ready_queue[i] == pthread_self())
-        {
-            ready_queue.erase(ready_queue.begin() + i);
-            break;
-        }
+    	if (ready_queue[i].tid == pthread_self())
+    	{
+    		printf("Removing %c%d\n", ready_queue[i].worker,ready_queue[i].id);
+    	    ready_queue.erase(ready_queue.begin() + i);
+    	    break;
+    	}
     }
     pthread_exit(0);
+}
+
+void* report(void *param)
+{
+	cout<<"ID:\t";
+	for(int i =0;i<ready_queue.size();i++)
+	{
+		printf("%c%d\t", ready_queue[i].worker, ready_queue[i].id);
+	}
+	printf("\n");
+	cout<<"Status:\t";
+	for(int i = 0; i<ready_queue.size();i++)
+	{
+		if(ready_queue[i].worker=='c')
+			printf("%d\t", status[ready_queue[i].id+num_p]);
+		// printf("Status:  %d\t", ready_queue[i].status);
+		else
+			printf("%d\t", status[ready_queue[i].id]);
+
+	}
+	printf("\n");
+	pthread_exit(0);
+
 }
 
 void* schedule(void* param)
 {
     int i;
     for (i = 0; i < num_p; i++)
+    {
+    	printf("Inside schedule. killing %d producer\n", i);
         pthread_kill(P_threads[i], SIGUSR1);
+    }
 
     for (i = 0; i < num_c; i++)
+    {
+    	printf("Inside schedule. killing %d consumer\n", i);
         pthread_kill(C_threads[i], SIGUSR1);
-
-    int ready_queue[N];
+    }
+    sleep(1);
 
     for (i = 0; i < num_p; i++)
-        ready_queue.push_back(P_threads[i]);
+    {
+    	threads thread;
+    	thread.tid = P_threads[i];
+    	thread.id = i;
+    	// thread.status = 0;
+    	thread.worker = 'p';
+        ready_queue.push_back(thread);
+    }
 
     for (i = 0; i < num_c; i++)
-        ready_queue.push_back(C_threads[i]);
+    {
+		threads thread;
+		thread.tid = C_threads[i];
+		thread.id = i;
+		// thread.status = 0;
+		thread.worker = 'c';
+	    ready_queue.push_back(thread);
+    }
 
     while(1)
     {
         if (!ready_queue.size())
             break;
-        pthread_t top = ready_queue[0];
+        pthread_t top = ready_queue[0].tid;
         pthread_kill(top, SIGUSR2);
+        printf("\nKilled %c%d\n",ready_queue[0].worker,ready_queue[0].id);
+        if(ready_queue[0].worker=='p')
+        {
+        	status[ready_queue[0].id] = 0;
+        }
+        else
+        {
+        	status[ready_queue[0].id+num_p] = 0;
+        }
+        // ready_queue[0].status = 0;
+        if(1)
+        {
+	        pthread_t reporter;
+	        pthread_attr_t attr_R;
+	        pthread_attr_init(&attr_R);
+	        pthread_create(&reporter,&attr_R,report,NULL); 
+	        pthread_join(reporter,NULL);
+	    }
+        threads thread = ready_queue[0];
         ready_queue.erase(ready_queue.begin());
-        ready_queue.push_back(top);
+        ready_queue.push_back(thread);
         sleep(QUANTUM);
         pthread_kill(top, SIGUSR1);
+        printf("Activated %c%d\n",ready_queue[0].worker,ready_queue[0].id);
+        // ready_queue[0].status = 1;
+        if(ready_queue[0].worker=='p')
+        {
+        	status[ready_queue[0].id] = 1;
+        }
+        else
+        {
+        	status[ready_queue[0].id+num_p] = 1;
+        }
+        if(1)
+        {
+	        pthread_t reporter;
+	        pthread_attr_t attr_R;
+	        pthread_attr_init(&attr_R);
+	        pthread_create(&reporter,&attr_R,report,NULL); 
+	        pthread_join(reporter,NULL);
+	    }
+        
     }
-    pthread_exit(0);
 
 }
+
+
+
 
 int main()
 {
@@ -119,8 +230,8 @@ int main()
     
     num_p = rand() % N;
     num_c = N - num_p;
+    cout<< "value of p: "<<num_p<<" value of C:"<<num_c<<endl;
     
-    pthread_t scheduler;
 
     for(int i = 0; i < num_p; i++)
     {
@@ -136,6 +247,7 @@ int main()
         pthread_create(&C_threads[i],&attr_C,consumer,NULL);          
     }
     pthread_create(&scheduler, NULL, schedule, NULL);
+    // pthread_create(&reporter, NULL, report, NULL);
 
     for(int i =0; i<num_p; i++)
     {
