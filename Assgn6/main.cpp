7e5bb9c30980b80019514441b1bf127359bfd8ea
americast <sayan.sinha@iitkgp.ac.in>
2019-04-07 10:07:55 +0530

    segfault :dissapointed:

--- Assgn6/main.cpp
+++ Assgn6/main.cpp
@@ -12,7 +12,7 @@ struct DIR
 
 struct super_block
 {
-	int sys_size, num_blocks;
+	int sys_size, num_blocks, block_size;
 	FAT* fat;
 	DIR* dir;
 	int* free;
@@ -27,7 +27,7 @@ struct block
 
 void* *blocks;
 
-void* my_open(char *file_name)
+block* my_open(char *file_name)
 {
 	int num_blocks = ((super_block *) blocks[0])->num_blocks;
 	int found = 0, pos;
@@ -58,6 +58,57 @@ void* my_open(char *file_name)
 	}
 }
 
+int my_write(block *file, char *text, int length)
+{
+	int block_size = ((super_block *) blocks[0])->block_size;
+	int count = 0;
+	do
+	{
+		int len_here, end_flag = 0;
+		if (length > block_size - sizeof(void *))
+			len_here = block_size - sizeof(void *);
+		else
+		{
+			len_here = length;
+			end_flag = 1;
+		}
+		file->buf = (char *) malloc(sizeof(char) * len_here);
+		memcpy(text + count, file->buf, len_here);
+		count+=len_here;
+		length-=len_here;
+		if (end_flag)
+		{
+			file->next_ptr = NULL;
+			break;
+		}
+		else
+		{
+			int i, found_flag = 0;
+			for (i = 0; i < ((super_block *)blocks[0])->num_blocks - 1; i++)
+			{
+				if (((super_block *)blocks[0])->free[i] == 0)
+				{
+					found_flag = 1;
+					break;
+				}
+			}
+			if (found_flag)
+			{
+				block *here = (block *) malloc(sizeof(block));;
+				blocks[i] = here;
+				file->next_ptr = here;
+				file = here;
+			}
+			else
+			{
+				fprintf(stderr, "Out of disk space\n");
+				return -1;
+			}
+		}
+	}while (length > 0);
+	return length;
+}
+
 int main()
 {
 	int sys_size, block_size;
@@ -75,10 +126,12 @@ int main()
 	sb->fat = (FAT *) malloc(sizeof(FAT) * (num_blocks - 1));
 	sb->free = (int *) malloc(sizeof(int) * (num_blocks- 1));
 	sb->sys_size = sys_size;
-	sb->num_blocks = block_size;
+	sb->block_size = block_size;
+	sb->num_blocks = num_blocks;
 
 	for (int i = 0; i < num_blocks; i++)
 		sb->free[i] = 0;
 
-	my_open("hello");
+	block *file = my_open("hello");
+	my_write(file, "text", 5);
 }
